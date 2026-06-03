d4eb24f2d3c6d380229a09b80a827ec1ad8de513
Lawrance Lui <lawranceluI@gmail.com>
2026-06-03 02:12:13 -0700

    🛠 Initial Project Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from util_file import new_json
+
+
+LOCAL_LISTINGS = new_json("DATA_GA/PRICING_GA/LOCAL_LISTINGS.json")
+LOCAL_SALES = new_json("DATA_GA/PRICING_GA/LOCAL_SALES.json")
+
+TARGET_LISTINGS = None  # Path to main repo LISTINGS.json
+TARGET_SALES = None     # Path to main repo SALES.json
+
+
+if __name__ == "__main__":
+    pass
